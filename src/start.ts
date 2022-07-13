bd2ad7e7d6cb5f09f7aa759dcbdedf6f8227d65c
SEFR <sebastien.francois@octo.com>
2022-07-13 08:45:47 +0200

    build[hello]: ajout du Procfile
    Ajouter un job dans le fichier Procfile

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,12 @@
-setInterval(() => console.info("Main app is alive..."), 3600000);
+import * as http from "http";
+
+const port = Number(process.env.PORT);
+const hostname = "0.0.0.0";
+
+const server = http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+	res.end("Hello World");
+});
+
+server.listen(port, hostname, () => console.info("Main app has started..."));
