4c6fac86f5dbbd8ada5a391b195a03a3a8b2d445
SEFR <sebastien.francois@octo.com>
2022-08-01 09:13:48 +0200

    build[main]: garder en vie main process

--- src/transformation/infrastructure/cron/transform-flux-jobteaser.cron.ts
+++ src/transformation/infrastructure/cron/transform-flux-jobteaser.cron.ts
@@ -16,9 +16,9 @@ export class TransformFluxJobteaserCron implements Cron {
 
 	init(): CronJob {
 		return new CronJob({
-			runOnInit: true,
-			start: true,
-			timeZone: "Europe/paris",
+			runOnInit: this.configuration.JOBTEASER.CRON_RUN_ON_INIT,
+			start: this.configuration.JOBTEASER.CRON_ENABLED,
+			timeZone: this.configuration.CRON_TIMEZONE,
 			onTick: (): void => {
 				const configurationFlux: ConfigurationFlux = {
 					nom: this.configuration.JOBTEASER.NAME,
@@ -27,13 +27,15 @@ export class TransformFluxJobteaserCron implements Cron {
 					dossierHistorisation: this.configuration.MINIO_HISTORY_DIRECTORY_NAME,
 				};
 
+				this.logger.info(`Start transformation of flux [${configurationFlux.nom}]`);
+
 				this.usecase.executer(configurationFlux)
 					.then(() => {
-						this.logger.info("Cron worked");
+						this.logger.info(`Flux [${configurationFlux.nom}] has been successfully transformed`);
 					})
 					.catch(e => this.logger.error(e));
 			},
-			cronTime: "* */10 * * *",
+			cronTime: this.configuration.JOBTEASER.CRON_TIME,
 		});
 	}
 }
