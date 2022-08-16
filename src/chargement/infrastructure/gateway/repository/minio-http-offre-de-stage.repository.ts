689f9d32b6e5d8e8a4dd88fc7170b2dd6451919d
SEFR <sebastien.francois@octo.com>
2022-08-16 12:16:38 +0200

    test[load]: ajout de test manquant sur l'offre de stage repository

--- src/chargement/infrastructure/gateway/repository/minio-http-offre-de-stage.repository.ts
+++ src/chargement/infrastructure/gateway/repository/minio-http-offre-de-stage.repository.ts
@@ -116,47 +116,29 @@ export class MinioHttpOffreDeStageRepository implements UnJeune1Solution.OffreDe
 		}
 	}
 
-	async createBucket(bucketName: string): Promise<void> {
-		const bucket = await this.minioClient.bucketExists(bucketName);
-
-		if (!bucket) {
-			await this.minioClient
-				.makeBucket(bucketName, "none");
-		}
-	}
-
-	async getObjectFromBucket(bucketName: string, objectName: string, targetFilePath: string): Promise<void> {
-		await this.minioClient.fGetObject(bucketName, objectName, targetFilePath);
-	}
-
-	listObjectsFromBucket(bucketName: string): void {
-		const stream = this.minioClient.listObjects(bucketName, "history/", true);
-		stream.on("data", (item) => console.info(item));
-	}
-
-	async setBucketLifecycle(bucketName: string, rule: object): Promise<void> {
-		await this.minioClient.setBucketLifecycle(bucketName, {
-			Rule: [{
-				Expiration: {
-					Days: 30,
-				},
-				ID: "history",
-				Filter: {
-					Prefix: "history/",
-				},
-				Status: "Enabled",
-			}, {
-				Expiration: {
-					Days: 130,
-				},
-				ID: "diff",
-				Filter: {
-					Prefix: "diff/",
-				},
-				Status: "Enabled",
-			},
-				rule,
-			],
-		});
-	}
+	// async setBucketLifecycle(bucketName: string, rule: object): Promise<void> {
+	// 	await this.minioClient.setBucketLifecycle(bucketName, {
+	// 		Rule: [{
+	// 			Expiration: {
+	// 				Days: 30,
+	// 			},
+	// 			ID: "history",
+	// 			Filter: {
+	// 				Prefix: "history/",
+	// 			},
+	// 			Status: "Enabled",
+	// 		}, {
+	// 			Expiration: {
+	// 				Days: 130,
+	// 			},
+	// 			ID: "diff",
+	// 			Filter: {
+	// 				Prefix: "diff/",
+	// 			},
+	// 			Status: "Enabled",
+	// 		},
+	// 			rule,
+	// 		],
+	// 	});
+	// }
 }
