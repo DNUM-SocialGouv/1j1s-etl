5c2d25a0077322a02ba0611979df67e6b63771bc
SEFR <sebastien.francois@octo.com>
2022-08-22 16:45:29 +0200

    fix[transformation]: fix dossier historisation stagefr-decompresse

--- src/transformation/infrastructure/tasks/transform-flux-stagefr-uncompressed.task.ts
+++ src/transformation/infrastructure/tasks/transform-flux-stagefr-uncompressed.task.ts
@@ -16,7 +16,7 @@ export class TransformFluxStagefrUncompressedTask implements Task {
 
 	async run(): Promise<void> {
 		const configurationFlux: ConfigurationFlux = {
-			dossierHistorisation: this.configuration.STAGEFR_UNCOMPRESSED.DIRECTORY_NAME,
+			dossierHistorisation: this.configuration.MINIO_HISTORY_DIRECTORY_NAME,
 			extensionFichierBrut: this.configuration.STAGEFR_UNCOMPRESSED.RAW_FILE_EXTENSION,
 			extensionFichierTransforme: this.configuration.STAGEFR_UNCOMPRESSED.TRANSFORMED_FILE_EXTENSION,
 			nom: this.configuration.STAGEFR_UNCOMPRESSED.NAME,
