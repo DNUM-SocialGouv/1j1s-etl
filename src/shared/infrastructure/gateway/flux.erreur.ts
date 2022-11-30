bf6434a6fb0a2c22da6fcae7dab4d77b7c2bfa19
SEFR <sebastien.francois@octo.com>
2022-11-30 15:30:52 +0100

    chore(evenements): ajout de virgules dans le scope de l'API Pôle Emploi

--- src/shared/infrastructure/gateway/flux.erreur.ts
+++ src/shared/infrastructure/gateway/flux.erreur.ts
@@ -5,7 +5,7 @@ export class EcritureFluxErreur extends Error {
 }
 
 export class LectureFluxErreur extends Error {
-	constructor(urlDuFlux: string) {
+	constructor(urlDuFlux: string, statusCode?: string) {
 		super(`Le flux à l'adresse ${urlDuFlux} n'a pas été extrait car une erreur de lecture est survenue`);
 	}
 }
