43626da6e8505f0880fa99d6c86596e111bfc2b7
SEFR <sebastien.francois@octo.com>
2023-01-07 18:58:20 +0100

    chore(architecture): ajout règle linter
    Ajout d'une règle de linter personnalisée pour contraindre les imports cross-context

---
+++ documentation/rules/index.ts
@@ -0,0 +1,24 @@
+import { NoCrossContextImportRule } from "./no-cross-context-import";
+
+module.exports = {
+	rules: {
+		"no-cross-context-import": {
+			create: NoCrossContextImportRule.create,
+			meta: {
+				docs: {
+					description: "Avoid cross-context import which can lead to strong coupling across the application",
+					recommended: "error",
+				},
+				type: "problem",
+			},
+		},
+	},
+	configs: {
+		architecture: {
+			plugins: ["1j1s"],
+			rules: {
+				"1j1s/no-cross-context-import": "error",
+			},
+		},
+	},
+};
