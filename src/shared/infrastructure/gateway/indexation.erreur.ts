96bf09fd38b0d8cfc4a306c9ce5d258fffaf3bbd
SEFR <sebastien.francois@octo.com>
2023-02-06 21:44:46 +0100

    feat(indexation): ajout configuration d'un index

--- src/shared/infrastructure/gateway/indexation.erreur.ts
+++ src/shared/infrastructure/gateway/indexation.erreur.ts
@@ -3,3 +3,9 @@ export class CreateIndexError extends Error {
 		super(`An error occured when trying to create index ${indexName}`);
 	}
 }
+
+export class IndexNotFoundError extends Error {
+	constructor(indexName: string) {
+		super(`Error with name ${indexName} doesn't exist`);
+	}
+}
--- test/shared/infrastructure/gateway/client/meilisearch-indexing.client.test.ts
+++ test/shared/infrastructure/gateway/client/meilisearch-indexing.client.test.ts
@@ -1,6 +1,6 @@
 import { MeilisearchIndexingClient } from "@shared/infrastructure/gateway/client/meilisearch-indexing.client";
+import { CreateIndexError, IndexNotFoundError } from "@shared/infrastructure/gateway/indexation.erreur";
 import { expect, StubbedClass, stubClass } from "@test/configuration";
-import { CreateIndexError } from "@shared/infrastructure/gateway/create-index.erreur";
 import { Index, MeiliSearch, MeiliSearchApiError } from "meilisearch";
 
 const indexName = "index-name";
@@ -18,58 +18,80 @@ describe("MeilisearchIndexingClientTest", () => {
 		meilisearchClient = new MeilisearchIndexingClient(meilisearch);
 	});
 
-	context("Lorsqu'un index existe déjà", () => {
-		it("n'essaie pas de recréer l'index", async () => {
-			// Given
-			meilisearch.getIndex
-				.withArgs(indexName)
-				.resolves(stubClass(Index));
+	context("Lorsqu'on souhaite créer un nouvel index", () => {
+		context("et que cet index existe déjà", () => {
+			it("n'essaie pas de recréer l'index", async () => {
+				// Given
+				meilisearch.getIndex
+					.withArgs(indexName)
+					.resolves(stubClass(Index));
 
-			// When
-			await meilisearchClient.createIndex(indexName);
+				// When
+				await meilisearchClient.createIndex(indexName);
 
-			// Then
-			expect(meilisearch.createIndex).to.not.have.been.called;
+				// Then
+				expect(meilisearch.createIndex).to.not.have.been.called;
+			});
 		});
-	});
 
-	context("Lorsqu'un index n'existe pas", () => {
-		it("crée un nouvel index", async () => {
-			// Given
-			meilisearch.getIndex
-				.withArgs(indexName)
-				.rejects(new MeiliSearchApiError({
-					type: "invalid_request",
-					code: "index_not_found",
-					link: "https://docs.meilisearch.com/errors#index_not_found",
-					message: "not found error",
-				}, 404));
+		context("et que cet index n'existe pas", () => {
+			it("crée un nouvel index", async () => {
+				// Given
+				meilisearch.getIndex
+					.withArgs(indexName)
+					.rejects(new MeiliSearchApiError({
+						type: "invalid_request",
+						code: "index_not_found",
+						link: "https://docs.meilisearch.com/errors#index_not_found",
+						message: "not found error",
+					}, 404));
 
-			// When
-			await meilisearchClient.createIndex(indexName);
+				// When
+				await meilisearchClient.createIndex(indexName);
 
-			// Then
-			expect(meilisearch.createIndex).to.have.been.calledOnce;
-			expect(meilisearch.createIndex).to.have.been.calledWith(indexName);
+				// Then
+				expect(meilisearch.createIndex).to.have.been.calledOnce;
+				expect(meilisearch.createIndex).to.have.been.calledWith(indexName);
+			});
 		});
-	});
 
-	context("Lorsqu'une erreur inattendue se produit lors de la récupération d'un index", () => {
-		it("lance une erreur", async () => {
-			// Given
-			meilisearch.getIndex
-				.withArgs(indexName)
-				.rejects(new Error("Oops! Something went wrong !"));
+		context("et qu'une erreur inattendue se produit lors de la récupération d'un index", () => {
+			it("lance une erreur", async () => {
+				// Given
+				meilisearch.getIndex
+					.withArgs(indexName)
+					.rejects(new Error("Oops! Something went wrong !"));
 
-			// When Then
-			await expect(meilisearchClient.createIndex(indexName)).to.have.been.rejectedWith(
-				CreateIndexError,
-				`An error occured when trying to create index ${indexName}`
-			);
+				// When Then
+				await expect(meilisearchClient.createIndex(indexName)).to.have.been.rejectedWith(
+					CreateIndexError,
+					`An error occured when trying to create index ${indexName}`,
+				);
+			});
 		});
 	});
 
 	context("Lorsqu'on indexe des documents", () => {
+		context("et que l'index n'existe pas", () => {
+			it("lance une erreur", async () => {
+				// Given
+				meilisearch.getIndex
+					.withArgs(indexName)
+					.rejects(new MeiliSearchApiError({
+						type: "invalid_request",
+						code: "index_not_found",
+						link: "https://docs.meilisearch.com/errors#index_not_found",
+						message: "not found error",
+					}, 404));
+
+				// When Then
+				await expect(meilisearchClient.index(indexName, [{ id: "1" }, { id: "2" }])).to.have.been.rejectedWith(
+					IndexNotFoundError,
+					`Error with name ${indexName} doesn't exist`,
+				);
+			});
+		});
+
 		it("récupère un index", async () => {
 			// When
 			await meilisearchClient.index(indexName, []);
@@ -91,5 +113,66 @@ describe("MeilisearchIndexingClientTest", () => {
 			);
 		});
 	});
-});
 
+	context("Lorsqu'on souhaite configurer un index", () => {
+		context("et que l'index n'existe pas", () => {
+			it("lance une erreur", async () => {
+				// Given
+				meilisearch.getIndex
+					.withArgs(indexName)
+					.rejects(new MeiliSearchApiError({
+						type: "invalid_request",
+						code: "index_not_found",
+						link: "https://docs.meilisearch.com/errors#index_not_found",
+						message: "not found error",
+					}, 404));
+
+				// When Then
+				await expect(meilisearchClient.configureIndex(indexName, {})).to.have.been.rejectedWith(
+					IndexNotFoundError,
+					`Error with name ${indexName} doesn't exist`,
+				);
+			});
+		});
+
+		context("et que l'index existe", () => {
+			it("configure les paramètres pour l'index", async () => {
+				// When
+				await meilisearchClient.configureIndex(indexName, {
+					faceting: {
+						maxValuesPerFacet: 5000,
+					},
+					displayedAttributes: ["id", "name"],
+					pagination: {
+						maxTotalHits: 100,
+					},
+					filterableAttributes: ["type", "price"],
+					searchableAttributes: ["description"],
+					typoTolerance: {
+						enabled: true,
+					},
+					sortableAttributes: ["type"],
+				});
+
+				// Then
+				const expectedSettings = {
+					faceting: {
+						maxValuesPerFacet: 5000,
+					},
+					displayedAttributes: ["id", "name"],
+					pagination: {
+						maxTotalHits: 100,
+					},
+					filterableAttributes: ["type", "price"],
+					searchableAttributes: ["description"],
+					typoTolerance: {
+						enabled: true,
+					},
+					sortableAttributes: ["type"],
+				};
+				expect(index.updateSettings).to.have.been.calledOnce;
+				expect(index.updateSettings).to.have.been.calledWith(expectedSettings);
+			});
+		});
+	});
+});
