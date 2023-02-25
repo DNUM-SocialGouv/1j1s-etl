a02e97d4ed64c4c6382a3a121c3fbe050126083d
SEFR <sebastien.francois@octo.com>
2023-02-25 11:23:46 +0100

    chore(stages): utilisation des modules NestJS dans la transformation
    Déplacement du CountryToIso dans le shared kernel

--- apps/stages/src/transformation/configuration/usecases.container.ts
+++ apps/stages/src/transformation/configuration/usecases.container.ts
@@ -1,6 +1,11 @@
-import { DateService } from "@shared/src/date.service";
+import { Module } from "@nestjs/common";
 
-import { UsecaseContainer } from "@stages/src/transformation/application-service";
+import { Shared } from "@shared/src";
+import { AssainisseurDeTexte } from "@shared/src/assainisseur-de-texte";
+import { DateService } from "@shared/src/date.service";
+import { CountryToIso } from "@shared/src/infrastructure/gateway/country-to-iso";
+import { Pays } from "@shared/src/pays";
+
 import {
 	TransformerFluxJobteaser,
 } from "@stages/src/transformation/application-service/transformer-flux-jobteaser.usecase";
@@ -10,43 +15,65 @@ import {
 import {
 	TransformerFluxStagefrDecompresse,
 } from "@stages/src/transformation/application-service/transformer-flux-stagefr-decompresse.usecase";
-import { Convertir as ConvertirJobteaser } from "@stages/src/transformation/domain/service/jobteaser/convertir.domain-service";
-import { Convertir as ConvertirStagefrCompresse } from "@stages/src/transformation/domain/service/stagefr-compresse/convertir.domain-service";
+import { Gateways } from "@stages/src/transformation/configuration/gateways.container";
+import {
+	Convertir as ConvertirJobteaser,
+} from "@stages/src/transformation/domain/service/jobteaser/convertir.domain-service";
+import { OffreDeStageRepository } from "@stages/src/transformation/domain/service/offre-de-stage.repository";
+import {
+	Convertir as ConvertirStagefrCompresse,
+} from "@stages/src/transformation/domain/service/stagefr-compresse/convertir.domain-service";
 import {
 	Convertir as ConvertirStagefrDecompresse,
 } from "@stages/src/transformation/domain/service/stagefr-decompresse/convertir.domain-service";
-import { GatewayContainer } from "@stages/src/transformation/infrastructure/gateway";
 
-export class UsecaseContainerFactory {
-	public static create(gateways: GatewayContainer): UsecaseContainer {
-		const dateService = new DateService();
-		const convertirOffreDeStageJobteaser = new ConvertirJobteaser(
-			dateService,
-			gateways.textSanitizer,
-			gateways.country
-		);
-		const convertirOffreDeStageStagefrCompresse = new ConvertirStagefrCompresse(
-			dateService,
-			gateways.textSanitizer
-		);
-		const convertirOffreDeStageStagefrDecompresse = new ConvertirStagefrDecompresse(
-			dateService,
-			gateways.textSanitizer
-		);
-
-		return {
-			transformerFluxJobteaser: new TransformerFluxJobteaser(
-				gateways.offreDeStageRepository,
-				convertirOffreDeStageJobteaser
-			),
-			transformerFluxStagefrCompresse: new TransformerFluxStagefrCompresse(
-				gateways.offreDeStageRepository,
-				convertirOffreDeStageStagefrCompresse
-			),
-			transformerFluxStagefrDecompresse: new TransformerFluxStagefrDecompresse(
-				gateways.offreDeStageRepository,
-				convertirOffreDeStageStagefrDecompresse,
-			),
-		};
-	}
+@Module({
+	imports: [Gateways, Shared],
+	providers: [
+		{
+			provide: ConvertirJobteaser,
+			inject: [DateService, "AssainisseurDeTexte", CountryToIso],
+			useFactory: (dateService: DateService, assainisseurDeTexte: AssainisseurDeTexte, pays: Pays): ConvertirJobteaser => {
+				return new ConvertirJobteaser(dateService, assainisseurDeTexte, pays);
+			},
+		},
+		{
+			provide: ConvertirStagefrCompresse,
+			inject: [DateService, "AssainisseurDeTexte"],
+			useFactory: (dateService: DateService, assainisseurDeTexte: AssainisseurDeTexte): ConvertirStagefrCompresse => {
+				return new ConvertirStagefrCompresse(dateService, assainisseurDeTexte);
+			},
+		},
+		{
+			provide: ConvertirStagefrDecompresse,
+			inject: [DateService, "AssainisseurDeTexte"],
+			useFactory: (dateService: DateService, assainisseurDeTexte: AssainisseurDeTexte): ConvertirStagefrDecompresse => {
+				return new ConvertirStagefrDecompresse(dateService, assainisseurDeTexte);
+			},
+		},
+		{
+			provide: TransformerFluxJobteaser,
+			inject: ["OffreDeStageRepository", ConvertirJobteaser],
+			useFactory: (offreDeStageRepository: OffreDeStageRepository, convertirOffreDeStage: ConvertirJobteaser): TransformerFluxJobteaser => {
+				return new TransformerFluxJobteaser(offreDeStageRepository, convertirOffreDeStage);
+			},
+		},
+		{
+			provide: TransformerFluxStagefrCompresse,
+			inject: ["OffreDeStageRepository", ConvertirJobteaser],
+			useFactory: (offreDeStageRepository: OffreDeStageRepository, convertirOffreDeStage: ConvertirStagefrCompresse): TransformerFluxStagefrCompresse => {
+				return new TransformerFluxStagefrCompresse(offreDeStageRepository, convertirOffreDeStage);
+			},
+		},
+		{
+			provide: TransformerFluxStagefrDecompresse,
+			inject: ["OffreDeStageRepository", ConvertirJobteaser],
+			useFactory: (offreDeStageRepository: OffreDeStageRepository, convertirOffreDeStage: ConvertirStagefrDecompresse): TransformerFluxStagefrDecompresse => {
+				return new TransformerFluxStagefrDecompresse(offreDeStageRepository, convertirOffreDeStage);
+			},
+		},
+	],
+	exports: [TransformerFluxJobteaser, TransformerFluxStagefrCompresse, TransformerFluxStagefrDecompresse],
+})
+export class Usecases {
 }
