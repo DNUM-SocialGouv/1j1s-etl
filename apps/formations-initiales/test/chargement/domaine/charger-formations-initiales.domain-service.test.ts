fac571565169b30ec595c4f585781bf199a71c20
julie BRUNETTO <julie.brunetto@octo.com>
2023-08-04 15:48:39 +0200

    feat(formations init): fin test domaine-service

--- apps/formations-initiales/test/chargement/domaine/charger-formations-initiales.domain-service.test.ts
+++ apps/formations-initiales/test/chargement/domaine/charger-formations-initiales.domain-service.test.ts
@@ -14,6 +14,8 @@ import {
 
 import { DateService } from "@shared/src/domain/service/date.service";
 
+import { OffreDeStageFixtureBuilder } from "@stages/test/chargement/fixture/offre-de-stage.fixture-builder";
+
 const maintenant = "2022-01-01T00:00:00.000Z";
 let nomDuFlux: string;
 let extensionDuFichierDeResultat: string;
@@ -21,6 +23,8 @@ let flux: FluxChargement;
 
 let formationsInitialesASupprimer: Array<UnJeuneUneSolution.FormationInitialeASupprimer>;
 let formationsInitialesASauvegarder: Array<UnJeuneUneSolution.FormationInitialeASauvegarder>;
+let formationsInitialesASauvegarderEnErreur: Array<UnJeuneUneSolution.FormationInitialeEnErreur>;
+let formationsInitialesASupprimerEnErreur: Array<UnJeuneUneSolution.FormationInitialeEnErreur>;
 
 let dateService: StubbedClass<DateService>;
 let formationsInitialesRepository: StubbedType<FormationsInitialesChargementRepository>;
@@ -40,38 +44,65 @@ describe("ChargerFormationsInitialesDomainServiceTest", () => {
 		domainService = new ChargerFormationsInitialesDomainService(formationsInitialesRepository, dateService);
 	});
 
-		context("Lorsque l'on charge l‘ensemble des formations intiales", () => {
-			beforeEach(() => {
-				formationsInitialesASupprimer = [FormationsInitialesFixtureBuilder.buildFormationsInitialesASupprimer(), FormationsInitialesFixtureBuilder.buildFormationsInitialesASupprimer({
-					identifiant: "id2",
-					intitule: "Patissier",
-				}, "id2")];
+	context("Lorsque l'on charge l‘ensemble des formations intiales", () => {
+		beforeEach(() => {
+			formationsInitialesASupprimer = [FormationsInitialesFixtureBuilder.buildFormationsInitialesASupprimer(), FormationsInitialesFixtureBuilder.buildFormationsInitialesASupprimer({
+				identifiant: "id2",
+				intitule: "Patissier",
+			}, "id2")];
+			formationsInitialesASauvegarder = [FormationsInitialesFixtureBuilder.buildFormationsInitialesASauvegarder(), FormationsInitialesFixtureBuilder.buildFormationsInitialesASauvegarder({
+				identifiant: "id2",
+				intitule: "Patissier a sauvegarder",
+			})];
 
-				formationsInitialesASauvegarder = [FormationsInitialesFixtureBuilder.buildFormationsInitialesASauvegarder(), FormationsInitialesFixtureBuilder.buildFormationsInitialesASauvegarder({
-					identifiant: "id2",
-					intitule: "Patissier a sauvegarder",
-				})];
+			formationsInitialesASupprimerEnErreur = [FormationsInitialesFixtureBuilder.buildFormationsInitialesEnErreur({
+				identifiant: "idASupprimerEnErreur",
+				intitule: "Intituler formation a supprimer en erreur",
+			})];
+			formationsInitialesASauvegarderEnErreur = [FormationsInitialesFixtureBuilder.buildFormationsInitialesEnErreur({
+				identifiant: "idASauvegarderEnErreur",
+				intitule: "Intituler formation a sauvegarder en erreur",
+			})];
 
-				formationsInitialesRepository.recupererFormationsInitialesASupprimer.resolves(formationsInitialesASupprimer);
-				formationsInitialesRepository.supprimer.resolves([]);
-				formationsInitialesRepository.recupererFormationsInitialesASauvegarder.resolves(formationsInitialesASauvegarder);
-				formationsInitialesRepository.enregistrerDansLeMinio.resolves([]);
-			});
-
-			it("Je charge les formations intiales", async () => {
-				await domainService.charger(flux);
-
-				expect(formationsInitialesRepository.recupererFormationsInitialesASupprimer).to.have.been.calledOnce;
-				expect(formationsInitialesRepository.recupererFormationsInitialesASupprimer).to.have.been.calledWith(nomDuFlux);
-
-				expect(formationsInitialesRepository.supprimer).to.have.been.calledOnce;
-				expect(formationsInitialesRepository.supprimer).to.have.been.calledWith(formationsInitialesASupprimer);
-
-				expect(formationsInitialesRepository.recupererFormationsInitialesASauvegarder).to.have.been.calledOnce;
-				expect(formationsInitialesRepository.recupererFormationsInitialesASauvegarder).to.have.been.calledWith(nomDuFlux);
-
-				expect(formationsInitialesRepository.chargerLesFormationsInitialesDansLeCMS).to.have.been.calledOnce;
-				expect(formationsInitialesRepository.chargerLesFormationsInitialesDansLeCMS).to.have.been.calledWith(formationsInitialesASauvegarder);
-			});
+			formationsInitialesRepository.recupererFormationsInitialesASupprimer.resolves(formationsInitialesASupprimer);
+			formationsInitialesRepository.supprimer.resolves(formationsInitialesASupprimerEnErreur);
+			formationsInitialesRepository.recupererFormationsInitialesASauvegarder.resolves(formationsInitialesASauvegarder);
+			formationsInitialesRepository.chargerLesFormationsInitialesDansLeCMS.resolves(formationsInitialesASauvegarderEnErreur);
+			formationsInitialesRepository.enregistrerDansLeMinio.resolves(Promise<void>);
 		});
+
+		it("Je charge les formations intiales", async () => {
+			const nomDuFichierASauvegarder = `${nomDuFlux}/${maintenant}_${extensionDuFichierDeResultat}`;
+			const nomDuFichierEnErreur = `${nomDuFlux}/${maintenant}_ERROR_${extensionDuFichierDeResultat}`;
+			const formationsInitialesEnErreur = formationsInitialesASupprimerEnErreur.concat(formationsInitialesASauvegarderEnErreur)
+
+			await domainService.charger(flux);
+
+			expect(formationsInitialesRepository.recupererFormationsInitialesASupprimer).to.have.been.calledOnce;
+			expect(formationsInitialesRepository.recupererFormationsInitialesASupprimer).to.have.been.calledWith(nomDuFlux);
+
+			expect(formationsInitialesRepository.supprimer).to.have.been.calledOnce;
+			expect(formationsInitialesRepository.supprimer).to.have.been.calledWith(formationsInitialesASupprimer);
+
+			expect(formationsInitialesRepository.recupererFormationsInitialesASauvegarder).to.have.been.calledOnce;
+			expect(formationsInitialesRepository.recupererFormationsInitialesASauvegarder).to.have.been.calledWith(nomDuFlux);
+
+			expect(formationsInitialesRepository.chargerLesFormationsInitialesDansLeCMS).to.have.been.calledOnce;
+			expect(formationsInitialesRepository.chargerLesFormationsInitialesDansLeCMS).to.have.been.calledWith(formationsInitialesASauvegarder);
+
+			expect(formationsInitialesRepository.enregistrerDansLeMinio).to.have.been.calledTwice;
+			expect(formationsInitialesRepository.enregistrerDansLeMinio.getCall(0).args).to.have.deep.members([
+				nomDuFichierASauvegarder,
+				JSON.stringify(
+					formationsInitialesASauvegarder, null, 2),
+				nomDuFlux,
+			]);
+			expect(formationsInitialesRepository.enregistrerDansLeMinio.getCall(1).args).to.have.deep.members([
+				nomDuFichierEnErreur,
+				JSON.stringify(
+					formationsInitialesEnErreur, null, 2),
+				nomDuFlux,
+			]);
+		});
+	});
 });
--- apps/formations-initiales/test/chargement/fixture/formations-initiales-builder.ts
+++ apps/formations-initiales/test/chargement/fixture/formations-initiales-builder.ts
@@ -1,4 +1,5 @@
 import { UnJeuneUneSolution } from "@formations-initiales/src/chargement/domain/model/1jeune1solution";
+import FormationInitiale = UnJeuneUneSolution.FormationInitiale;
 
 export class FormationsInitialesFixtureBuilder {
 	static DEFAULTS_ATTRIBUTS = {
@@ -7,7 +8,7 @@ export class FormationsInitialesFixtureBuilder {
 		duree: "2 ans",
 		certification: "3",
 		niveauEtudesVise: "Bac + 2",
-		description: "description de la formation" ,
+		description: "description de la formation",
 		attendusParcoursup: "L‘option managament d‘unité de production culinaire vise à maîtriser des techniques culinaires propres aux différents types de restauration",
 		conditionsAcces: "Le diplomé peut débuter comme chef de partie, second de cuisine, avant d‘accéder à des postes d‘encadrement ou de direction.",
 		poursuiteEtudes: "Le BTS est un diplôme conçu pour une insertion professionnelle",
@@ -26,5 +27,15 @@ export class FormationsInitialesFixtureBuilder {
 			{ ...FormationsInitialesFixtureBuilder.DEFAULTS_ATTRIBUTS, ...formationInitiale },
 			id || this.DEFAULT_ID,
 		);
+	}
+
+	static buildFormationsInitialesEnErreur(
+		formationInitiale?: Partial<UnJeuneUneSolution.FormationInitiale>,
+		motif?: string,
+	): UnJeuneUneSolution.FormationInitialeEnErreur {
+		return {
+			motif: motif ?? "Une erreur est survenue lors de l‘action demandée",
+			formationInitiale: new FormationInitiale({ ...FormationsInitialesFixtureBuilder.DEFAULTS_ATTRIBUTS, ...formationInitiale }),
+		};
 	}
 }
