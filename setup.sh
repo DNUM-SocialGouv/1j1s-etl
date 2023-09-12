ee8db11a196e637834c9eef1c9bf80e698f32e69
suli-octo <129731991+suli-octo@users.noreply.github.com>
2023-09-12 16:29:37 +0200

    chore(git-notify): removed (#354)

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ if [[ -z $NODE_ENV ]];then
 
     cp ./hooks/* .git/hooks/
 
-    chmod +x .git/hooks/post-merge;
-    chmod +x .git/hooks/post-rewrite;
-    chmod +x .git/hooks/post-checkout;
     chmod +x .git/hooks/commit-msg;
     exit 0;
 fi
