9a8898dc6bc9df03deceb5096193bfbc11b7f8f5
julie BRUNETTO <julie.brunetto@octo.com>
2023-12-29 11:44:14 +0100

    tech(supertest): fix linter

--- apps/api/test/app.e2e-spec.ts
+++ apps/api/test/app.e2e-spec.ts
@@ -2,11 +2,12 @@ import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 
 import request from "supertest";
+import { App } from "supertest/types";
 
 import { ApiModule } from "@api/src/api.module";
 
 describe("ApiController (e2e)", () => {
-  let app: INestApplication;
+  let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
